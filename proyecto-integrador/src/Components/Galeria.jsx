da8666f9e34c462a890a3a8c3b7fbf8f64c9afab
davidlamp810 <davidl.lamprea810@gmail.com>
2024-08-08 16:40:00 -0500

    actualización home

--- proyecto-integrador/src/Components/Galeria.jsx
+++ proyecto-integrador/src/Components/Galeria.jsx
@@ -1,6 +1,7 @@
 // src/Components/Galeria.jsx
 // eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
+import '../Styles/Galeria.css'; // Importa los estilos CSS
 
 const images = [
   { src: '/product_img/boda_grande.jpg', title: 'Boda Grande' },
@@ -30,71 +31,39 @@ const Galeria = () => {
   const [shuffledImages, setShuffledImages] = useState([]);
 
   useEffect(() => {
-    const shuffled = shuffleArray(images);
-    setShuffledImages(shuffled.slice(0, 9)); 
+    const handleResize = () => {
+      const width = window.innerWidth;
+      let numberOfImages = 8; // Default for desktop and tablets
+
+      if (width <= 480) {
+        numberOfImages = 4; // For mobile devices
+      }
+      const shuffled = shuffleArray(images);
+      setShuffledImages(shuffled.slice(0, numberOfImages));
+    };
+
+    handleResize(); // Set images based on initial screen size
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return (
-    <div style={galleryStyle}>
+    <div className="gallery">
       {shuffledImages.map((image, index) => (
-        <div key={index} style={imageCardStyle(index)}>
-          <img src={image.src} alt={`img-${index}`} style={imageStyle} />
-          <div style={imageInfoStyle}>
-            <h2 style={titleStyle}>{image.title}</h2>
-            <p style={textStyle}>20 personas</p>
-            <p style={textStyle}>Comida a elección standard</p>
-            <p style={textStyle}>1 postre</p>
-            <p style={textStyle}>1 mozo</p>
+        <div key={index} className="image-card">
+          <img src={image.src} alt={`img-${index}`} />
+          <div className="image-info">
+            <h2>{image.title}</h2>
+            <p>20 personas</p>
+            <p>Comida a elección standard</p>
+            <p>1 postre</p>
+            <p>1 mozo</p>
           </div>
         </div>
       ))}
     </div>
   );
-};
-
-const galleryStyle = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gap: '20px',
-  '@media (max-width: 768px)': {
-    gridTemplateColumns: 'repeat(2, 1fr)',
-    gap: '10px',
-  },
-  '@media (max-width: 480px)': {
-    gridTemplateColumns: '1fr',
-    gap: '10px',
-  },
-};
-
-const imageCardStyle = (index) => ({
-  display: index >= 4 && window.innerWidth <= 480 ? 'none' : 'flex',
-  flexDirection: 'column',
-  border: '1px solid #ddd',
-  borderRadius: '10px',
-  overflow: 'hidden',
-  backgroundColor: 'white',
-  color: 'black',
-  margin: '5px',
-});
-
-const imageStyle = {
-  width: '100%',
-  height: '200px',
-  objectFit: 'cover',
-};
-
-const imageInfoStyle = {
-  padding: '10px',
-  textAlign: 'center',
-};
-
-const titleStyle = {
-  margin: '0',
-  fontSize: '18px',
-};
-
-const textStyle = {
-  margin: '5px 0',
 };
 
 export default Galeria;
--- proyecto-integrador/src/Routes/Home.jsx
+++ proyecto-integrador/src/Routes/Home.jsx
@@ -2,22 +2,19 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Galeria from '../Components/Galeria';
+import '../Styles/Home.css'; // Importa los estilos
 
 function Home() {
     return (
-<<<<<<< HEAD
-        <main className="">
-            <h1>Recomendados</h1>
+        <main className="main">
+            <h1 className="title">Recomendados</h1>
             <Galeria />
         </main>
     );
-=======
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
->>>>>>> 0cdc9695570717667e855163549cb0da4ea2f4c4
 }
 
 export default Home;
+
+
+
 
