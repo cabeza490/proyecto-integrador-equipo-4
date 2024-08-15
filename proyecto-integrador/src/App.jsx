e2181fa811969e25460f9ff119836c8f7e3b71f6
santigonzalezarrospide <santiago.g.a16@gmail.com>
2024-08-15 19:55:25 -0300

    avances de estilos de formulario registro

--- proyecto-integrador/src/App.jsx
+++ proyecto-integrador/src/App.jsx
@@ -3,21 +3,25 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './Contexts/AuthContext';
-import Navbar from './Components/Navbar';
 import Home from './Routes/Home';
 import Login from './Routes/Login';
 import Register from './Routes/Register';
+import Layout from './Layout/Layout';
+
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Navbar />
+
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="*" element={<h1>404 not found</h1>} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<h1>404 not found</h1>} />
+          </Route>
+
         </Routes>
       </AuthProvider>
     </BrowserRouter>
--- proyecto-integrador/src/Routes/Register.jsx
+++ proyecto-integrador/src/Routes/Register.jsx
@@ -1,11 +1,9 @@
-// src/Routes/Register.jsx
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import FormRegister from '../Components/RegisterForm';
 
 const Register = () => {
   return (
-    <div className="register-container">
+    <div>
       <FormRegister />
     </div>
   );
