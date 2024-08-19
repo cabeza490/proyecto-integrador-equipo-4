94753e5f95f1ac36fbf388d5f84ff7a22ff2f502
santigonzalezarrospide <santiago.g.a16@gmail.com>
2024-08-19 19:04:15 -0300

    avances esteticos de caracteristicas

--- proyecto-integrador/src/Components/Detail.jsx
+++ proyecto-integrador/src/Components/Detail.jsx
@@ -1,41 +1,80 @@
-import { useState, useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
-import { getProductoById } from '../api/productos-Apis'
-import '../Styles/Detail.css'
+import { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { getProductoById } from '../api/productos-Apis';
+import '../Styles/Detail.css';
 
 const Detail = () => {
-    const {id} = useParams()
-    const [productSelected, setProductSelected] = useState({})
+    const { id } = useParams();
+    const [productSelected, setProductSelected] = useState({});
 
     useEffect(() => {
-        const getData = async() => {
+        const getData = async () => {
             let getProductData = await getProductoById(id);
-            setProductSelected(getProductData)
-        } 
-        getData()
-    }, [id])
-    console.log(productSelected);
+            setProductSelected(getProductData);
+        };
+        getData();
+    }, [id]);
 
-    return(
-        <>
-            <div className='card_container' key={productSelected.id}>
-
+    return (
+        <div className="content">
+            <div className="card_container" key={productSelected.id}>
                 <div className="card_title">
-                    <h2>{productSelected.nombre}</h2>
+                    <h2>{productSelected.nombre || 'Título del servicio'}</h2>
                     <Link to={`/`}>Volver al Inicio</Link>
                 </div>
-                <div className='card_content'>
+                <div className="card_content">
                     <div className="card_image">
-                        {productSelected.imagenes && productSelected.imagenes.length > 0 && (
+                        {productSelected.imagenes && productSelected.imagenes.length > 0 ? (
                             <img src={productSelected.imagenes[0].url} alt={productSelected.nombre} />
-                            )}
+                        ) : (
+                            <div className="placeholder_image"></div>
+                        )}
                     </div>
-                    <p>{productSelected.descripcion}</p>
+                    <p>{productSelected.descripcion || 'Descripción del servicio.'}</p>
                 </div>
             </div>
-        </>
 
-    )
-}
+            <div className="card_container">
+                <h3>Más detalles</h3>
+                <p>Aquí irá más información que se pueda tomar desde la BBDD, como el precio.</p>
+            </div>
 
-export default Detail
+            <div className="card_container">
+                <h3>Características</h3>
+                <div className='caracteristicas'>
+                    <p>
+                        Apto vegano
+                    </p>
+                    
+                    <p>
+                        Personal calificado
+                    </p>
+                   
+                    <p>
+                        Característica
+                    </p>
+                    
+                    <p>
+                        Característica
+                    </p>
+                    <p>
+                        Apto celíaco
+                    </p>
+                    <p>
+                        Bebida incluida
+                    </p>
+                    <p>
+                        Característica
+                    </p>
+                    <p>
+                        Característica
+                    </p>
+                </div>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Detail;
