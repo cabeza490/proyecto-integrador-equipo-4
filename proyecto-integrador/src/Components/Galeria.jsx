ce9246d68dd7ca870f3e607d3ae8a7fd61b3fd0d
santigonzalezarrospide <santiago.g.a16@gmail.com>
2024-09-02 23:11:15 +0300

    filtrado por categoria

--- proyecto-integrador/src/Components/Galeria.jsx
+++ proyecto-integrador/src/Components/Galeria.jsx
@@ -1,63 +1,45 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import '../Styles/Galeria.css';
 import { Link } from 'react-router-dom';
 
-const Galeria = ({ searchTerm = '', setNoResults }) => {  // Agregar setNoResults
+const Galeria = ({ searchTerm = '', selectedCategories = [], setNoResults, setTotalResults }) => {
   const [shuffledImages, setShuffledImages] = useState([]);
 
   useEffect(() => {
-    const handleResize = () => {
-      const width = window.innerWidth;
-      let numberOfImages = 8; // Default for desktop and tablets
+    const fetchImages = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/productos');
+        const productos = response.data.productos;
 
-      if (width <= 480) {
-        numberOfImages = 4; // For mobile devices
+        // Filtrar por categorías seleccionadas
+        const filteredByCategory = selectedCategories.length > 0
+          ? productos.filter(producto => selectedCategories.includes(producto.categoria_id))
+          : productos;
+
+        const images = filteredByCategory.flatMap(producto => producto.imagenes.map(imagen => ({
+          src: imagen.url,
+          title: producto.nombre,
+          description: producto.descripcion,
+          id: producto.id
+        })));
+
+        const filteredImages = searchTerm
+          ? images.filter(image => image.title.toLowerCase().includes(searchTerm.toLowerCase()))
+          : images;
+
+        setShuffledImages(filteredImages);
+        setTotalResults(filteredImages.length);  // Actualizar el total de resultados
+        setNoResults(filteredImages.length === 0); // Actualizar estado si no hay resultados
+      } catch (error) {
+        console.error('Error al obtener las imágenes:', error);
+        setNoResults(true); // Si ocurre un error, también se considera que no hay resultados
       }
-
-      const fetchImages = async () => {
-        try {
-          const response = await axios.get('http://localhost:3000/api/productos');
-          const productos = response.data.productos;
-          const images = productos.flatMap(producto => producto.imagenes.map(imagen => ({
-            src: imagen.url,
-            title: producto.nombre,
-            description: producto.descripcion,
-            id: producto.id
-          })));
-          const shuffled = shuffleArray(images);
-          
-          const filteredImages = searchTerm
-            ? shuffled.filter(image => image.title.toLowerCase().includes(searchTerm.toLowerCase()))
-            : shuffled;
-
-          setShuffledImages(filteredImages.slice(0, numberOfImages));
-          setNoResults(filteredImages.length === 0); // Actualizar estado si no hay resultados
-        } catch (error) {
-          console.error('Error al obtener las imágenes:', error);
-          setNoResults(true); // Si ocurre un error, también se considera que no hay resultados
-        }
-      };
-
-      fetchImages();
     };
 
-    handleResize(); // Set images based on initial screen size
-    window.addEventListener('resize', handleResize);
-
-    return () => window.removeEventListener('resize', handleResize);
-  }, [searchTerm, setNoResults]); // Agregar setNoResults como dependencia
-
-  const shuffleArray = (array) => {
-    let shuffledArray = array.slice();
-    for (let i = shuffledArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-    }
-    return shuffledArray;
-  };
+    fetchImages();
+  }, [searchTerm, selectedCategories, setNoResults, setTotalResults]);
 
   return (
     <div className="gallery">
@@ -78,9 +60,9 @@ const Galeria = ({ searchTerm = '', setNoResults }) => {  // Agregar setNoResult
 
 Galeria.propTypes = {
   searchTerm: PropTypes.string.isRequired,
-  setNoResults: PropTypes.func.isRequired,  // Agregar PropTypes para setNoResults
+  selectedCategories: PropTypes.arrayOf(PropTypes.number),  // Aceptar un array de IDs de categorías
+  setNoResults: PropTypes.func.isRequired,
+  setTotalResults: PropTypes.func.isRequired,  // Prop para actualizar el total de resultados
 };
 
 export default Galeria;
-
-
--- proyecto-integrador/src/Routes/Home.jsx
+++ proyecto-integrador/src/Routes/Home.jsx
@@ -1,14 +1,15 @@
-// src/Routes/Home.jsx
-// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import Galeria from '../Components/Galeria';
 import '../Styles/Home.css';
 import Search from '../Components/Search';
+import Category from '../Components/Category';
 
 function Home() {
     const [searchTerm, setSearchTerm] = useState('');
     const [searchDate, setSearchDate] = useState(null);
-    const [noResults, setNoResults] = useState(false); // Nuevo estado para controlar si no hay resultados
+    const [noResults, setNoResults] = useState(false);
+    const [selectedCategories, setSelectedCategories] = useState([]); // Array de objetos de categorías seleccionadas
+    const [totalResults, setTotalResults] = useState(0);
 
     const formatDate = (date) => {
         if (!date) return '';
@@ -16,40 +17,87 @@ function Home() {
     };
 
     const handleSearch = () => {
-        // Aquí puedes agregar cualquier lógica adicional cuando se complete la búsqueda.
         console.log("Búsqueda realizada con éxito.");
+    };
+
+    const handleRemoveCategory = (categoryId) => {
+        const updatedCategories = selectedCategories.filter(cat => cat.id !== categoryId);
+        setSelectedCategories(updatedCategories); // Actualizar el estado con las categorías restantes
+    };
+
+    const handleClearAllFilters = () => {
+        setSelectedCategories([]);
+        setSearchTerm('');
+        setSearchDate(null);
+    };
+
+    const areFiltersApplied = () => {
+        return searchTerm || searchDate || selectedCategories.length > 0;
     };
 
     return (
         <main className="main">
             <Search setSearchTerm={setSearchTerm} setSearchDate={setSearchDate} onSearch={handleSearch} />
 
-            {(searchTerm || searchDate) && (
+            <Category onCategorySelect={setSelectedCategories} selectedCategories={selectedCategories} />
+
+            {/* Mostrar resultados filtrados solo si hay filtros aplicados */}
+            {areFiltersApplied() && (
                 <>
                     <h2 className="title">
-                        Resultados de tu búsqueda: {searchTerm}
-                        {searchDate && (
-                            <span className="search-date-text">
-                                : para la fecha: {formatDate(searchDate)}
-                            </span>
+                        {searchTerm || searchDate ? (
+                            <>
+                                Resultados de tu búsqueda: {searchTerm}
+                                {searchDate && (
+                                    <span className="search-date-text">
+                                        : para la fecha: {formatDate(searchDate)}
+                                    </span>
+                                )}
+                            </>
+                        ) : (
+                            <>
+                                {selectedCategories.length > 0 && (
+                                    <span className="search-category-text">
+                                        {selectedCategories.map((category, index) => (
+                                            <span key={category.id} className="selected-category">
+                                                {category.nombre}
+                                                <button onClick={() => handleRemoveCategory(category.id)} className="remove-filter-button">✕</button>
+                                                {index < selectedCategories.length - 1 && ', '}
+                                            </span>
+                                        ))}
+                                        <span className="result-count">
+                                            {`Visualizando ${totalResults} resultados`}
+                                        </span>
+                                    </span>
+                                )}
+                            </>
                         )}
                     </h2>
 
-                    {noResults ? ( // Mostrar mensaje si no hay resultados
+                    {noResults ? (
                         <p className="no-results-message">No se encontró ningún producto</p>
                     ) : (
-                        <Galeria searchTerm={searchTerm} setNoResults={setNoResults} />
+                        <Galeria
+                            searchTerm={searchTerm}
+                            selectedCategories={selectedCategories.map(cat => cat.id)}  // Pasar solo los IDs de categorías seleccionadas
+                            setNoResults={setNoResults}
+                            setTotalResults={setTotalResults}  // Actualiza el total de resultados
+                        />
                     )}
                 </>
             )}
 
-            <h1 className="title">Recomendados</h1>
-            <div style={{ marginTop: '20px' }}>
-                <Galeria searchTerm="" />
-            </div>
+            {/* Mostrar Recomendados solo si NO hay filtros aplicados */}
+            {!areFiltersApplied() && (
+                <>
+                    <h1 className="title">Recomendados</h1>
+                    <div style={{ marginTop: '20px' }}>
+                        <Galeria searchTerm="" selectedCategories={[]} /> {/* Recomendados sin filtros */}
+                    </div>
+                </>
+            )}
         </main>
     );
 }
 
 export default Home;
-
