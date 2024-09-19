b1cff84e997d448fbc5d5c6468a39e59d8c163f1
davidlamp810 <davidl.lamprea810@gmail.com>
2024-09-18 22:31:41 -0500

    historia 33

--- proyecto-integrador/src/Components/HistorialReservas.jsx
+++ proyecto-integrador/src/Components/HistorialReservas.jsx
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 import { useCateringStates } from '../Components/utils/globalContext';
+import { Link } from 'react-router-dom';
 import '../Styles/HistorialReservas.css';
 
 // Obtener la URL base del backend desde las variables de entorno
@@ -53,27 +53,35 @@ const HistorialReservas = () => {
                     ) : reservas.length === 0 ? (
                         <p>No tienes reservas en el historial</p>
                     ) : (
-                        reservas.map((reserva, index) => (
-                            <div key={index} className="reserva-card">
-                                <img
-                                    src={reserva.producto && reserva.producto.imagenes && reserva.producto.imagenes.length > 0
-                                        ? reserva.producto.imagenes[0].url
-                                        : 'default-image.jpg'}
-                                    alt={`reserva-${index}`}
-                                />
-                                <div className="reserva-info">
-                                    <h3>{reserva.producto ? reserva.producto.nombre : 'Nombre no disponible'}</h3>
-                                    <p>{reserva.producto ? reserva.producto.descripcion : 'Descripción no disponible'}</p>
-                                    <p><strong>Fecha de reserva:</strong> {reserva.fecha_uso || 'Fecha no disponible'}</p>
-                                    <p><strong>Precio:</strong> ${reserva.producto ? reserva.producto.precio : 'Precio no disponible'}</p>
-                                    <div className="reserva-buttons">
-                                        <Link to={`/detail/${reserva.producto ? reserva.producto.id : ''}`} className="detail-button">
-                                            Ver detalle
+                        reservas.map((reserva, index) => {
+                            console.log('Reserva:', reserva); // Verifica los datos aquí
+                            return (
+                                <div key={index} className="reserva-card-wrapper">
+                                    <div className="reserva-header">
+                                        <h3>Fecha de reserva: {reserva.fecha_reserva || 'Fecha no disponible'}</h3>
+                                    </div>
+                                    <div className="reserva-card">
+                                        <img
+                                            src={reserva.producto && reserva.producto.imagenes && reserva.producto.imagenes.length > 0
+                                                ? reserva.producto.imagenes[0].url
+                                                : 'default-image.jpg'}
+                                            alt={`reserva-${index}`}
+                                        />
+                                        <div className="reserva-info">
+                                            <h3>{reserva.producto ? reserva.producto.nombre : 'Nombre no disponible'}</h3>
+                                            <p>{reserva.producto ? reserva.producto.descripcion : 'Descripción no disponible'}</p>
+                                            <p><strong>Fecha de uso del servicio:</strong> {reserva.fecha_uso || 'Fecha no disponible'}</p>
+                                            <p><strong>Precio:</strong> ${reserva.producto ? reserva.producto.precio : 'Precio no disponible'}</p>
+                                            <div className="reserva-buttons">
+                                        <Link to={`/detail/${reserva.producto_id}`} className="detail-button">
+                                            Ver detalles
                                         </Link>
                                     </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        ))
+                            );
+                        })
                     )}
                 </div>
             </section>
